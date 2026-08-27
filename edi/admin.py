cbcb81e74c1b390c55b47f8c02a90a5c0d48b19d
Hansavahini <hansavahinib@gmail.com>
2026-08-27 13:18:55 +0530

    changes with excel generation work correctly

--- edi/admin.py
+++ edi/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ProcessingJob
 
-# Register your models here.
+@admin.register(ProcessingJob)
+class ProcessingJobAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'kind', 'state', 'progress', 'created_at')
+    list_filter = ('kind', 'state')
+    search_fields = ('owner__username', 'message')
